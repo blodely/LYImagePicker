ab161ce73fe042b45568aa97d679d617746a3bdc
Luo Yu (MacBook Pro) <indie.luo@gmail.com>
2017-06-14 10:33:39 +0800

    [Modify] demo project

--- Example/LYImagePicker/AppDelegate.swift
+++ Example/LYImagePicker/AppDelegate.swift
@@ -16,6 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+		
+		window = UIWindow.init(frame: UIScreen.main.bounds)
+		
+		let testvc = TestViewController.init(nibName: "TestViewController", bundle: Bundle.main)
+		let testnav = UINavigationController.init(rootViewController: testvc);
+		
+		window?.rootViewController = testnav
+		
+		window?.makeKeyAndVisible()
+		
         return true
     }
 
---
+++ Example/LYImagePicker/TestViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TestViewController.swift
+//  LYImagePicker
+//
+//  Created by 駱彧 on 14/6/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class TestViewController: UIViewController {
+	
+	override func loadView() {
+		super.loadView()
+		
+		self.navigationItem.title = "Test (LYImagePicker)"
+	}
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
