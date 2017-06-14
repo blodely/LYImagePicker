b6fd417b192dd48876c0fef8e7e16dfd41a71a47
Luo Yu (MacBook Pro) <indie.luo@gmail.com>
2017-06-14 11:33:29 +0800

    [Add] image picker instance

---
+++ LYImagePicker/Classes/LYImagePicker.swift
@@ -0,0 +1,30 @@
+//
+//  LYImagePicker.swift
+//  LYIMAGEPICKER
+//
+//  CREATED BY LUO YU ON 2017-06-14.
+//  COPYRIGHT © 2017 LUO YU. ALL RIGHTS RESERVED.
+//
+
+import UIKit
+
+class LYImagePicker: NSObject {
+	
+	// MARK: - SHARED INSTANCE
+	
+	static let shared: LYImagePicker = {
+		
+		let instance = LYImagePicker()
+		
+		return instance
+		
+	}()
+	
+	// MARK: - INIT
+	
+	override init() {
+		super.init()
+	}
+	
+	// MARK:
+}
