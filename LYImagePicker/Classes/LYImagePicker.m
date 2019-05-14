3bce7902ef03600e96e7bc7cfb95fcec86c9d847
Luo Yu <indie.luo@gmail.com>
2019-05-14 20:10:30 +0800

    [Modify] remove sort

--- LYImagePicker/Classes/LYImagePicker.m
+++ LYImagePicker/Classes/LYImagePicker.m
@@ -80,10 +80,10 @@ NSString *const LIB_IMAGE_PICKER_BUNDLE_ID = @"org.cocoapods.LYImagePicker";
 	
 	PHFetchOptions *options = [[PHFetchOptions alloc] init];
 	options.includeHiddenAssets = YES;
-	options.sortDescriptors = @[
-								[NSSortDescriptor sortDescriptorWithKey:@"modificationDate" ascending:NO],
-//								[NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:NO],
-								];
+//	options.sortDescriptors = @[
+//								[NSSortDescriptor sortDescriptorWithKey:@"modificationDate" ascending:NO],
+//								];
+	// SORT KEYS: modificationDate creationDate
 	
 	NSMutableArray<PHAsset *> *assets = [NSMutableArray arrayWithCapacity:1];
 	PHFetchResult *result = [PHAsset fetchAssetsWithOptions:options];
